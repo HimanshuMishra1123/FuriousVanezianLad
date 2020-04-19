b165f4ab2805cfa940fbcde8a6d115f8193a38ae
HimanshuMishra1123 <63342464+HimanshuMishra1123@users.noreply.github.com>
2020-04-19 15:11:21 +0530

    Update furiousvanezianladbot.py

--- furiousvanezianladbot.py
+++ furiousvanezianladbot.py
@@ -6,10 +6,10 @@ REPLY_MESSAGE = "Melone, I am on the FUCKING edge right now! You're trying to sa
 
 def authenticate():
     print("Authenticating...")
-    reddit = praw.Reddit(client_id = config.Client_id,
-                    client_secret = config.Client_secret,
-                    username = config.Username,
-                    password = config.Password,
+    reddit = praw.Reddit(client_id = config.client_id,
+                    client_secret = config.client_secret,
+                    username = config.username,
+                    password = config.password,
                     user_agent = 'FuriousVanezianLad by /u/FuriousVanezianLad')
     print("Authenticated as {}".format(reddit.user.me()))
     return reddit
